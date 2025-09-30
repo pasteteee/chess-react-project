413b91456cc76366966d82be2b8e41ee1974a237
Pavel Ivanov <pavel.meiruz@gmail.com>
2025-09-30 14:22:57 +0300

    add dragging animations in the best project ever

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -2,20 +2,37 @@ import styles from "./Cell.module.scss";
 import { Figure } from "../Figure/Figure";
 import { useEffect, useState } from "react";
 
-export const Cell = ({ index, value, pices, validMoves, clickEvent, prevMove, activeCell }) => {
-    const [isUnderlined, setUnderlined] = useState(false);
+export const Cell = ({
+  index,
+  value,
+  pices,
+  validMoves,
+  clickEvent,
+  prevMove,
+  activeCell,
+}) => {
+  const pointClasses = ["", styles.symple, styles.beat, styles.symple];
 
-    const pointClasses = ['', styles.symple, styles.beat, styles.symple];
+  const [isUnderlined, setUnderlined] = useState(false);
 
-    return (
-        <div
-            onClick={(e) => clickEvent(e, index)}
-            onAuxClick={() => setUnderlined((prev) => !prev)}
-            className={`${styles.cell} ${isUnderlined ? styles.underlined : ''}`}   
-            color_type={value.color}
-        >
-            <div className={`${styles.point} ${pointClasses[validMoves[index]]}`}></div>
-            {value.figure && <Figure value={value.figure} pices={pices} prevMove={prevMove} isAnimated={activeCell === index}/>}
-        </div>
-    );
+  return (
+    <div
+      onClick={(e) => clickEvent(e, index)}
+      onAuxClick={() => setUnderlined((prev) => !prev)}
+      className={`${styles.cell} ${isUnderlined ? styles.underlined : ""}`}
+      color_type={value.color}
+    >
+      <div
+        className={`${styles.point} ${pointClasses[validMoves[index]]}`}
+      ></div>
+      {value.figure && (
+        <Figure
+          value={value.figure}
+          pices={pices}
+          prevMove={prevMove}
+          isAnimated={activeCell === index}
+        />
+      )}
+    </div>
+  );
 };
--- src/components/Figure/Figure.jsx
+++ src/components/Figure/Figure.jsx
@@ -2,23 +2,93 @@ import { useEffect, useState, useRef } from "react";
 import styles from "./Figure.module.scss";
 import { getCoordinates } from "../../utils/additional";
 
-export const Figure = ({value, pices, prevMove, isAnimated}) => {
-    const figureImg = useRef();
-    const [coord, setCoord] = useState({x: 0, y: 0});
+export const Figure = ({ value, pices, prevMove, isAnimated }) => {
+  const figureImg = useRef();
+  const ghostRef = useRef();
+  const [coord, setCoord] = useState({ x: 0, y: 0 });
 
-    useEffect(() => {
-        setCoord(prev => ({...prev, x: figureImg.current.x, y: figureImg.current.y}))
-    }, [isAnimated])
-    
-    return (
-        <div className={`${styles.figure} ${isAnimated ? styles.isAnimated : ''}`}  style={{
-                    '--figure-transfer-translate-x':  `${prevMove.x - coord.x}px`,
-                    '--figure-transfer-translate-y':  `${prevMove.y - coord.y}px`,
-                }} draggable>
-            <img ref={figureImg}
-                src={`/pices/${pices}/${value.color[0]}${value.sym}.png`}
-                
-            />
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragPosition, setDragPosition] = useState({x: 0, y: 0})
+  const [originalPosition, setOriginalPosition] = useState({ x: 0, y: 0 });
+
+  const startDragging = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+
+    const rect = figureImg.current.getBoundingClientRect();
+    setOriginalPosition({ x: rect.left, y: rect.top });
+
+    setDragPosition({
+      x: e.clientX - rect.width / 2,
+      y: e.clientY - rect.height / 2,
+    });
+
+    const handleMouseMove = (moveEvent) => {
+      setDragPosition(prev => ({
+        ...prev,
+        x: moveEvent.clientX - rect.width / 2,
+        y: moveEvent.clientY - rect.height / 2,
+      }));
+    };
+
+    const handleMouseUp = () => {
+      setIsDragging(false);
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  useEffect(() => {
+    setCoord((prev) => ({
+      ...prev,
+      x: figureImg.current.x,
+      y: figureImg.current.y,
+    }));
+  }, [isAnimated]);
+
+  return (
+    <div
+      className={`${styles.figure} ${isAnimated ? styles.isAnimated : ""}`}
+      style={{
+        "--figure-transfer-translate-x": `${prevMove.x - coord.x}px`,
+        "--figure-transfer-translate-y": `${prevMove.y - coord.y}px`,
+      }}
+      draggable
+    >
+      <img
+        onMouseDown={startDragging}
+        ref={figureImg}
+        style={{
+          opacity: isDragging ? 0.3 : 1
+        }}
+        src={`/pices/${pices}/${value.color[0]}${value.sym}.png`}
+      />
+
+      {isDragging && (
+        <div
+          ref={ghostRef}
+          style={{
+            width: `100px`,
+            height: `100px`,
+            display: 'block',
+            position: 'fixed',
+            left: dragPosition.x,
+            top: dragPosition.y,
+            pointerEvents: 'none',
+            cursor: 'grabbing',
+            padding: '10px',
+            zIndex: 1000,
+            transform: 'rotate(5deg) scale(1.05) translate(-25%, -25%)',
+          }}
+        >
+          <img
+            src={`/pices/${pices}/${value.color[0]}${value.sym}.png`}
+          />
         </div>
-    );
+      )}
+    </div>
+  );
 };
